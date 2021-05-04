9a149c4ff6201bd3f9e6dd961cc7434d8ef79c26
vozsiberiana <70288823+vozsiberiana@users.noreply.github.com>
2021-05-04 14:31:39 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from "react";
 import "./App.css";
 import Ticket from "./Ticket";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ninebarc app</h1>
         <Ticket />
+        <p>
+          This project was coded by Anna Knyazkina and it's{" "}
+          <a
+            href="https://github.com/vozsiberiana/ninebarc-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
       </header>
     </div>
   );
 }
-
-export default App;
