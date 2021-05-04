da2f300392fa925b7782b38c8e65583644e6a1f9
vozsiberiana <70288823+vozsiberiana@users.noreply.github.com>
2021-05-04 15:37:10 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import Ticket from "./Ticket";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Ninebarc app</h1>
-        <Ticket />
-        <p>
-          This project was coded by Anna Knyazkina and it's{" "}
-          <a
-            href="https://github.com/vozsiberiana/ninebarc-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Ninebarc app</h1>
+          <Ticket />
+          <p>
+            This project was coded by Anna Knyazkina and it's{" "}
+            <a
+              href="https://github.com/vozsiberiana/ninebarc-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </header>
+      </div>
     </div>
   );
 }
